4677206eface7b190d1b00171fd162116912491a
victor <victor.lissoviy.93@gmail.com>
2025-08-10 10:23:51 +0300

    remove repositories and tests from Calculator module

--- Calculator/build.gradle.kts
+++ Calculator/build.gradle.kts
@@ -10,18 +10,9 @@ plugins {
 	java
 }
 
-repositories {
-	mavenCentral()
-}
-
 // Apply a specific Java toolchain to ease working on different environments.
 java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(21)
 	}
-}
-
-tasks.named<Test>("test") {
-	// Use JUnit Platform for unit tests.
-	useJUnitPlatform()
 }
