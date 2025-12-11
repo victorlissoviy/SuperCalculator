63a55e23ed5f7ae2263f48b2d724dd51611831c9
victor <victor.lissoviy.93@gmail.com>
2025-12-11 05:25:15 +0200

    remove unnecessary comments

--- operations/divide/build.gradle.kts
+++ operations/divide/build.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
-	// Apply the application plugin to add support for building a CLI application in Java.
 	id("java")
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
 java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(25)
