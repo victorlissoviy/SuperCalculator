4b2fb191fb824229444a9a835e2e665238d38cd7
victor <victor.lissoviy.93@gmail.com>
2026-04-19 06:28:38 +0300

    modify test cases and dependencies for operations tests

--- CalculatorTest/build.gradle.kts
+++ CalculatorTest/build.gradle.kts
@@ -11,6 +11,10 @@ repositories {
 
 dependencies {
 	testImplementation(project(":Calculator"))
+	testImplementation(project(":operations:plus"))
+	testImplementation(project(":operations:minus"))
+	testImplementation(project(":operations:multi"))
+	testImplementation(project(":operations:divide"))
 	testImplementation(platform("org.junit:junit-bom:5.10.0"))
 	testImplementation("org.junit.jupiter:junit-jupiter")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- backend/src/test/java/org/victor/calculator/controllers/CalcControllerTest.java
+++ backend/src/test/java/org/victor/calculator/controllers/CalcControllerTest.java
@@ -13,7 +13,9 @@ import org.victor.calculator.operations.OperationResponse;
 import org.victor.calculator.services.CalcService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
